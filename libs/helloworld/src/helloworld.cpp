8572ea3298c7a51533ad64b83275db0acfddbc16
Matthew Crowell <78377986+Cahlien@users.noreply.github.com>
2026-02-17 02:06:01 -0600

    doc: Document C++ classes with QDoc comments

--- libs/helloworld/src/helloworld.cpp
+++ libs/helloworld/src/helloworld.cpp
@@ -1,12 +1,36 @@
 #include "helloworld.h"
 
+/*!
+    \class HelloWorld
+    \inmodule HelloWorld
+    \brief A simple greeting class that provides a "Hello World" message.
+
+    The HelloWorld class stores a greeting message and provides
+    accessors for retrieving it in raw or formatted form.
+*/
+
+/*!
+    Constructs a HelloWorld object with the default message "Hello World".
+*/
 HelloWorld::HelloWorld() : m_message("Hello World") {}
 
+/*!
+    Returns the stored message as a string view.
+
+    \return A \c std::string_view referencing the internal message string.
+*/
 std::string_view HelloWorld::message() const noexcept
 {
     return m_message;
 }
 
+/*!
+    Returns the message wrapped in a formatted string.
+
+    The format is: \c{Message: '<message>'}.
+
+    \return A \c std::string containing the formatted message.
+*/
 std::string HelloWorld::formatMessage() const
 {
     return "Message: '" + m_message + "'";
--- src/main/common/app_info.cpp
+++ src/main/common/app_info.cpp
@@ -1,6 +1,21 @@
 #include "app_info.h"
 #include <QString>
 
+/*!
+    \class AppInfo
+    \inqmlmodule QtQuickTemplate
+    \brief Singleton exposing application metadata to QML.
+
+    AppInfo is registered as a QML singleton via \c QML_ELEMENT and
+    \c QML_SINGLETON. It provides read-only properties such as
+    \l version for display in the UI.
+*/
+
+/*!
+    \property AppInfo::version
+    The application version string, set at compile time from
+    \c APP_VERSION_STRING.
+*/
 QString AppInfo::version() const
 {
     return QStringLiteral(APP_VERSION_STRING);
