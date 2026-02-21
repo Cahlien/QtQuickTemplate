92ad717f4ae5dafc2ed31ee8b6ab47a5346abdce
Matthew Crowell <78377986+Cahlien@users.noreply.github.com>
2026-02-20 23:26:54 -0600

    fix: enforce release signing identity in Xcode settings

--- cmake/platform/AppleCodeSigning.cmake
+++ cmake/platform/AppleCodeSigning.cmake
@@ -26,29 +26,29 @@ function(configure_apple_release_code_signing target)
 
     set_target_properties(${target} PROPERTIES
         XCODE_ATTRIBUTE_PRODUCT_BUNDLE_IDENTIFIER ${QTQUICKTEMPLATE_APPLE_BUNDLE_IDENTIFIER}
-        XCODE_ATTRIBUTE_DEVELOPMENT_TEAM[variant=Release] ${QTQUICKTEMPLATE_APPLE_DEVELOPMENT_TEAM}
-        XCODE_ATTRIBUTE_CODE_SIGNING_ALLOWED[variant=Release] YES
-        XCODE_ATTRIBUTE_CODE_SIGNING_REQUIRED[variant=Release] YES
+        XCODE_ATTRIBUTE_DEVELOPMENT_TEAM ${QTQUICKTEMPLATE_APPLE_DEVELOPMENT_TEAM}
+        XCODE_ATTRIBUTE_CODE_SIGNING_ALLOWED YES
+        XCODE_ATTRIBUTE_CODE_SIGNING_REQUIRED YES
     )
 
     if (IOS)
         set_target_properties(${target} PROPERTIES
-            XCODE_ATTRIBUTE_CODE_SIGN_STYLE[variant=Release] Automatic
+            XCODE_ATTRIBUTE_CODE_SIGN_STYLE Automatic
             XCODE_ATTRIBUTE_TARGETED_DEVICE_FAMILY "1,2"
-            XCODE_ATTRIBUTE_PROVISIONING_PROFILE_SPECIFIER[variant=Release] ""
+            XCODE_ATTRIBUTE_PROVISIONING_PROFILE_SPECIFIER ""
         )
     else ()
         if (QTQUICKTEMPLATE_MACOS_APP_SIGN_IDENTITY)
             set_target_properties(${target} PROPERTIES
-                XCODE_ATTRIBUTE_CODE_SIGN_STYLE[variant=Release] Manual
-                XCODE_ATTRIBUTE_CODE_SIGN_IDENTITY[variant=Release] ${QTQUICKTEMPLATE_MACOS_APP_SIGN_IDENTITY}
+                XCODE_ATTRIBUTE_CODE_SIGN_STYLE "$<$<CONFIG:Release>:Manual>$<$<NOT:$<CONFIG:Release>>:Automatic>"
+                XCODE_ATTRIBUTE_CODE_SIGN_IDENTITY "$<$<CONFIG:Release>:${QTQUICKTEMPLATE_MACOS_APP_SIGN_IDENTITY}>"
             )
         else ()
             message(WARNING
                 "QTQUICKTEMPLATE_MACOS_APP_SIGN_IDENTITY is not set; macOS release signing will use Automatic style and may pick Apple Development."
             )
             set_target_properties(${target} PROPERTIES
-                XCODE_ATTRIBUTE_CODE_SIGN_STYLE[variant=Release] Automatic
+                XCODE_ATTRIBUTE_CODE_SIGN_STYLE Automatic
             )
         endif ()
     endif ()
